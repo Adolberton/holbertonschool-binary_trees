6bb18b254aae335d17411cc35a09371e7d259060
Adolberton <7557@holbertonstudents.com>
2024-01-10 12:03:49 +0100

    new func

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,6 +6,10 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	if (!tree->parent)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
 
 	size = binary_tree_size(tree->left) + binary_tree_size(tree->right);
 
