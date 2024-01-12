887ecfefddf9b580a8e0c97a088772dbb181d359
Adolberton <7557@holbertonstudents.com>
2024-01-12 21:02:57 +0100

    fix

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,17 +2,17 @@
 
 /**
  * binary_tree_uncle - finds 'uncle' to a node, sibling to `node` parent
-* @node: node to check
-* Return: pointer to sibling node of parent to `node`, or NULL if none found
-* or `node` is NULL
-*/
+ * @node: node to check
+ * Return: pointer to sibling node of parent to `node`, or NULL if none found
+ * or `node` is NULL
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent;
-
-	if (!node || !node->parent)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	parent = node->parent;
-	return (binary_tree_sibling(parent));
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
 }
