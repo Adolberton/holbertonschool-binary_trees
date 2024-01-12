77663c55ccb059e42c360048dcee9291c97379e0
Adolberton <7557@holbertonstudents.com>
2024-01-12 20:59:48 +0100

    new func

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,19 +1,28 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree to check
- * Return: 0 if tree = NULL or 1 if perfect
+ * binary_tree_is_perfect - checks if a binary tree is perfect, meaning all
+ * nodes except for leaves have two children
+ * @tree: root node below which to check
+ * Return: 1 if `tree` and all nodes below it each have 2 children, 0 if not or
+ * if `tree` is NULL
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t nodes = 0, height = 0, perfect = 0;
-
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	nodes = binary_tree_nodes(tree);
-	height = binary_tree_height(tree);
-	perfect = _pow_recursion(2, height + 1) - 1;
-	if (nodes == perfect)
+
+	if (tree->right && tree->left)
+	{
+		if ((!tree->left->left && !tree->left->right) &&
+		    (!tree->right->left && !tree->right->right))
+			return (1);
+
+		return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
+	}
+	else if (!(tree->parent))
 		return (1);
+
 	return (0);
 }
